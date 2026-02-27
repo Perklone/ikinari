40594b49c8aeaa38572747411555370e436be2a6
Rizky Maulana <71255973+Perklone@users.noreply.github.com>
2026-02-27 22:14:06 +0700

    color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./_site/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'silver': '#c0c5ce',
+        'red-accent': '#b83a3a',
+        'dark-gray': '#2d3142',
+      },
       fontFamily: {
         'dmserif': ['"DM Serif Display"', "serif"],
         'ebgaramond': ['EB Garamond', "serif"],
